6835631f59ba5cdd015bec4c09fda278da18de11
Angelo Assis <aangelo6974@gmail.com>
2026-02-03 12:10:27 -0300

    refactor to use per-vertex color and add hot-reload to shaders

--- shaders/test.frag
+++ shaders/test.frag
@@ -1,9 +1,9 @@
 #version 410
 
-uniform vec4 inputColor;
-out vec4 fragColor;
+in vec3 color;
+out vec4 frag_color;
 
 void main()
 {
-	fragColor = inputColor;
+	frag_color = vec4(color, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <assert.h>
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
@@ -19,6 +20,34 @@ int g_win_height = 480;
 
 int g_fb_width = 640;
 int g_fb_height = 480;
+
+GLfloat points[] = {
+	0.0f, 0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	-0.5f, -0.5f, 0.0f
+};
+
+const char* GL_type_to_string(GLenum type)
+{
+	switch (type)
+	{
+	case GL_BOOL: return "bool";
+	case GL_INT: return "int";
+	case GL_FLOAT: return "float";
+	case GL_FLOAT_VEC2: return "vec2";
+	case GL_FLOAT_VEC3: return "vec3";
+	case GL_FLOAT_VEC4: return "vec4";
+	case GL_FLOAT_MAT2: return "mat2";
+	case GL_FLOAT_MAT3: return "mat3";
+	case GL_FLOAT_MAT4: return "mat4";
+	case GL_SAMPLER_2D: return "sampler2D";
+	case GL_SAMPLER_3D: return "sampler3D";
+	case GL_SAMPLER_CUBE: return "samplerCube";
+	case GL_SAMPLER_2D_SHADOW: return "sampler2DShadow";
+	default: break;
+	}
+	return "other";
+}
 
 void glfw_window_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -53,18 +82,6 @@ void _update_fps_counter(GLFWwindow* window)
 	frame_count++;
 }
 
-// read shader from external file
-std::string read_shader_file(const std::string& path)
-{
-	std::ifstream file;
-	file.open(path);
-
-	std::stringstream buffer;
-	buffer << file.rdbuf();
-
-	return buffer.str();
-}
-
 void _print_shader_info_log(GLuint shader_index)
 {
 	int max_length = 2048;
@@ -83,6 +100,149 @@ void _print_program_info_log(GLuint program)
 	printf("program info log for GL index %u:\n%s\n", program, log);
 }
 
+void print_all(GLuint program)
+{
+	printf("------------------------\nshader program %i info:\n", program);
+	int params = -1;
+	glGetProgramiv(program, GL_LINK_STATUS, &params);
+	printf("GL_LINK_STATUS = %i\n", params);
+
+	glGetProgramiv(program, GL_ATTACHED_SHADERS, &params);
+	printf("GL_ATTACHED_SHADERS = %i\n", params);
+
+	glGetProgramiv(program, GL_ACTIVE_ATTRIBUTES, &params);
+	printf("GL_ACTIVE_ATTRIBUTES = %i\n", params);
+
+	for (GLuint i = 0; i < (GLuint)params; i++)
+	{
+		char name[64];
+		int max_length = 64;
+		int actual_length = 0;
+		int size = 0;
+		GLenum type;
+		glGetActiveAttrib(program, i, max_length, &actual_length, &size, &type, name);
+
+		if (size > 1)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				char long_name[64];
+				sprintf(long_name, "%s[%i]", name, j);
+				int location = glGetAttribLocation(program, long_name);
+				printf(" %i) type:%s name:%s location:%i\n", i, GL_type_to_string(type), long_name, location);
+			}
+		}
+		else
+		{
+			int location = glGetAttribLocation(program, name);
+			printf(" %i) type:%s name:%s location:%i\n", i, GL_type_to_string(type), name, location);
+		}
+	}
+
+	glGetProgramiv(program, GL_ACTIVE_UNIFORMS, &params);
+	printf("GL_ACTIVE_UNIFORMS = %i\n", params);
+	for (GLuint i = 0; i < (GLuint)params; i++)
+	{
+		char name[64];
+		int max_length = 64;
+		int actual_length = 0;
+		int size = 0;
+		GLenum type;
+		glGetActiveUniform(program, i, max_length, &actual_length, &size, &type, name);
+		
+		if (size > 1)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				char long_name[64];
+				sprintf(long_name, "%s[%i]", name, j);
+				int location = glGetUniformLocation(program, long_name);
+				printf(" %i) type:%s name:%s location:%i\n", i, GL_type_to_string(type), long_name, location);
+			}
+		}
+		else
+		{
+			int location = glGetUniformLocation(program, name);
+			printf(" %i) type:%s name:%s location:%i\n", i, GL_type_to_string(type), name, location);
+		}
+	}
+
+	_print_program_info_log(program);
+}
+
+bool is_valid(GLuint program)
+{
+	glValidateProgram(program);
+	int params = -1;
+	glGetProgramiv(program, GL_VALIDATE_STATUS, &params);
+	printf("program %i GL_VALIDATE_STATUS = %i\n", program, params);
+	if (GL_TRUE != params)
+	{
+		_print_program_info_log(program);
+		return false;
+	}
+	return true;
+}
+
+// read shader from external file
+std::string read_shader_file(const std::string& path)
+{
+	std::ifstream file;
+	file.open(path);
+
+	std::stringstream buffer;
+	buffer << file.rdbuf();
+
+	return buffer.str();
+}
+
+GLuint load_shader_program(const char* vertPath, const char* fragPath)
+{
+	std::string vertexCode = read_shader_file(vertPath);
+	std::string fragmentCode = read_shader_file(fragPath);
+
+	const char* vsrc = vertexCode.c_str();
+	const char* fsrc = fragmentCode.c_str();
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vsrc, NULL);
+	glCompileShader(vs);
+
+	GLint success;
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		_print_shader_info_log(vs);
+		return 0;
+	}
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fsrc, NULL);
+	glCompileShader(fs);
+
+	glGetShaderiv(fs, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		_print_shader_info_log(fs);
+		return 0;
+	}
+
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (!success) {
+		_print_program_info_log(program);
+		glDeleteProgram(program);
+		return 0;
+	}
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
 int main() {
 	glfwSetErrorCallback(glfw_error_callback);
 
@@ -92,6 +252,7 @@ int main() {
 		return 1;
 	}
 
+	restart_gl_log();
 	gl_log("starting GLFW: {}\n", glfwGetVersionString());
 
 	glfwWindowHint(GLFW_SAMPLES, 8);
@@ -128,17 +289,31 @@ int main() {
 		-0.5f, -0.5f, 0.0f
 	};
 
-	GLuint vbo = 0;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	GLfloat colors[] = {
+	1.0f, 0.0f, 0.0f,
+	0.0f, 1.0f, 0.0f,
+	0.0f, 0.0f, 1.0f
+	};
+
+	GLuint points_vbo = 0;
+	glGenBuffers(1, &points_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+	GLuint colors_vbo = 0;
+	glGenBuffers(1, &colors_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
 
 	GLuint vao = 0;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
 
 	std::string vertexCode = read_shader_file("shaders/test.vert");
 	std::string fragmentCode = read_shader_file("shaders/test.frag");
@@ -172,23 +347,12 @@ int main() {
 		return false;
 	}
 
-	GLuint shader_program = glCreateProgram();
-	glAttachShader(shader_program, fs);
-	glAttachShader(shader_program, vs);
-	glLinkProgram(shader_program);
-
-	// check for linking errors
-	glGetProgramiv(shader_program, GL_LINK_STATUS, &params);
-	if (GL_TRUE != params)
+	GLuint shader_program = load_shader_program("shaders/test.vert", "shaders/test.frag");
+	if (!shader_program)
 	{
-		fprintf(stderr, "ERROR: could not link shader program GL index %u\n", shader_program);
-		_print_program_info_log(shader_program);
-		return false;
+		fprintf(stderr, "ERROR: could not load shaders\n");
+		return 1;
 	}
-
-	int vertexColorLocation = glGetUniformLocation(shader_program, "inputColor");
-	glUseProgram(shader_program);
-	glUniform4f(vertexColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
 
 	glfwSetWindowSizeCallback(window, glfw_window_size_callback);
 	glfwSetFramebufferSizeCallback(window, glfw_framebuffer_resize_callback);
@@ -203,6 +367,38 @@ int main() {
 		glViewport(0, 0, g_fb_width, g_fb_height);
 		glUseProgram(shader_program);
 		glBindVertexArray(vao);
+
+		static bool reloadPressed = false;
+		if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
+		{
+			if (!reloadPressed)
+			{
+				GLuint new_program = load_shader_program("shaders/test.vert", "shaders/test.frag");
+
+				if (new_program)
+				{
+					glDeleteProgram(shader_program);
+					shader_program = new_program;
+					glUseProgram(shader_program);
+
+					printf("Shaders successfully reloaded.\n");
+				}
+				else
+				{
+					printf("Failed to reload shaders.\n");
+				}
+				reloadPressed = true;
+			}
+		}
+		else
+		{
+			reloadPressed = false;
+		}
+
+		// winding and back-face culling
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_BACK);
+		glFrontFace(GL_CW);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
